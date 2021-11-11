e86e1c60ac9695ea9a98a029ed25781461ee67d2
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-11 13:19:14 -0800

    [Swetha] add : Hormonic number Program added

---
+++ CorePrograms/CorePrograms/HarmonicNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class HarmonicNumber
+    {
+        /// <summary>
+        /// This method is responsible to print the nth hormonic number
+        /// </summary>
+        public void FindNumber()
+        {
+            double harmonicNumber = 0;
+            Console.WriteLine("Enter the Harmonic Number as N Where N Should not be 0");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                harmonicNumber += 1 / (float)i;
+            }
+            Console.WriteLine(Math.Round(harmonicNumber, 2));
+        }
+    }
+}
