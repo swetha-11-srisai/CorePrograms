9f5267be27b16b66237fcc37e03b5311b9932c4b
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-11 13:12:10 -0800

    [Swetha] add : Power of Two Program added

---
+++ CorePrograms/CorePrograms/PowerOfTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class PowerOfTwo
+    {
+		int power(int bas, int exponent)
+		{
+			int power = 1;
+			for (int i = 1; i <= exponent; i++)
+				power = power * bas;
+			return power;
+		}
+		public void Calculate()
+		{
+			int bas = 2;
+			Console.WriteLine("Enter the Power No.");
+			int exponent = Convert.ToInt32(Console.ReadLine());
+			int pow = power(bas, exponent);
+			Console.WriteLine(bas + "to the power of" + exponent + "is : " + pow);
+		}
+	}
+}
