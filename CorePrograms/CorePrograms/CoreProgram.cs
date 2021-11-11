15d82733abb697677fe624b60c2c041c1b1840b3
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-11 12:52:12 -0800

    [Swetha] add : flip the coin

---
+++ CorePrograms/CorePrograms/CoreProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class CoreProgram
+    {
+        public void CoinFlip()
+        {
+            Console.WriteLine("Enter the Number of times want to flip the coin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int headCount = 0, tailCount = 0;
+            double percentage;
+            for (int i = 0; i < num; i++)
+            {
+                double flip = random.NextDouble();
+                if (flip < 0.5)
+                {
+                    Console.WriteLine("Heads");
+                    headCount++;
+                    percentage = (headCount / num) * 100;
+                    Console.WriteLine("Percentage of Heads vs Tails is: " + percentage);
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    tailCount++;
+                    percentage = (tailCount / num) * 100;
+                    Console.WriteLine("Percentage of Heads vs Tails is: " + percentage);
+                }
+
+            }
+        }
+    }
+}
+
